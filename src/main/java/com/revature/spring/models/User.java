cd43c1d57da150a2771f763387e0feb00a219326
Adam Lyn <adam273@revature.net>
2022-03-09 11:50:43 -0500

    Added rough UserRepository

--- src/main/java/com/revature/spring/models/User.java
+++ src/main/java/com/revature/spring/models/User.java
@@ -1,10 +1,116 @@
 package com.revature.spring.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.revature.models.UserRole;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ERS_USERS")
 public class User {
+
+    @Id
+    private String user_id;
+
+    @Column(nullable = false)
+    private String given_name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean isActive;
+
+    @ManyToOne()
+    @JoinColumn(name = "ROLE_ID")
+    private UserRole role;
+
+    public User() {
+        super();
+    }
+
+    public User(String user_id, String given_name, String surname, String email, String username,
+                String password, Boolean isActive, UserRole role) {
+        this.user_id = user_id;
+        this.given_name = given_name;
+        this.surname = surname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.isActive = isActive;
+        this.role = role;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getGiven_name() {
+        return given_name;
+    }
+
+    public void setGiven_name(String given_name) {
+        this.given_name = given_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
